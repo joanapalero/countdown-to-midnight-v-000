42d8d40e296c440827d18e7be1d2d35fb76ab44e
Joana Palero <joana.palero24@gmail.com>
2018-05-27 19:43:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,5 +7,13 @@ until time_left == 0
   time_left -= 1
 end
   HAPPY NEW YEAR!
-  end
 end
+
+def countdown_with_sleep(time)
+  time_left = time
+    until time_left == 0
+      puts "#{time_left} SECOND(S)!"
+      time_left -= 1
+      sleep 5
+    end
+  end
