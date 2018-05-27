4ff1905e6041ab59d98b4602f8f33cc29138ed65
Joana Palero <joana.palero24@gmail.com>
2018-05-27 19:41:37 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 #write your code here
 
-def countdown(number)
-  number = 1
-while number < 10
-sleep 5
-  puts "#{number} SECOND(S)!"
+def countdown(time)
+  time_left = time
+until time_left == 0
+  puts "#{time_left} SECOND(S)!"
+  time_left -= 1
+end
   HAPPY NEW YEAR!
   end
 end
